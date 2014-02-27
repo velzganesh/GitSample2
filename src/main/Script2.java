5c0390a4b8e9915276201a3d812cc4bca62acff5
vganesh <velganesh_ashokkumaar@trimble.com>
2014-02-27 18:34:42 +0530

    changes 4 to 5

--- src/main/Script2.java
+++ src/main/Script2.java
@@ -4,7 +4,7 @@ public class Script2 {
 
 	public static void main(String[] args) {
 		System.out.println("Sample Git Script 3!!!");
-		System.out.println("Sample Git Script 4!!!");
+		System.out.println("Sample Git Script 5!!!");
 	}
 
 }
