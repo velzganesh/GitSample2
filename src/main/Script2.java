e16bfbf552b9f04bcfa9f751b4df3270812d3471
vganesh <velganesh_ashokkumaar@trimble.com>
2014-02-27 18:31:07 +0530

    initial commit
    initial

---
+++ src/main/Script2.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Script2 {
+
+	public static void main(String[] args) {
+		System.out.println("Sample Git Script 3!!!");
+		System.out.println("Sample Git Script 4!!!");
+	}
+
+}
